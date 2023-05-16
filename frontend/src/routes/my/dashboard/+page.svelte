1af2f3a78af122b0ff6c07c95b9344792c6dfe84
Puria Nafisi Azizi <puria@dyne.org>
2023-05-16 15:25:17 +0200

    remove frontend

--- frontend/src/routes/my/dashboard/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Signroom dashboard</h1>
--- frontend/src/routes/my/projects/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let projects;
-</script>
-
-<h1 class="text-6xl">miaam</h1>
-
-<h1>My projects</h1>
-<pre>{JSON.stringify(projects, null, 2)}</pre>
