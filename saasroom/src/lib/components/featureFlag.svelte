bbab58e10ab0c5c22bf6437ff8468cfa0d2fc00c
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-18 11:05:07 +0200

    feat(sr): created FeatureFlag component

---
+++ saasroom/src/lib/components/featureFlag.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { featureFlags } from '$lib/pocketbase';
+	import { devLog } from '$lib/utils/devLog';
+
+	export let flag: string;
+
+	let flagRecord = $featureFlags.find((f) => f.name == flag);
+	if (!flagRecord) devLog('flagNotFound:', flag);
+</script>
+
+{#if flagRecord && flagRecord.active}
+	<slot />
+{/if}
