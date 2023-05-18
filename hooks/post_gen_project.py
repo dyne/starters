b63fe8f38c93ae10982923949c540444c5abfa00
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-18 21:10:27 +0200

    feat(cookiecutter): completed hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,17 +3,16 @@ import shutil
 
 def createENV():
     # Get the base directory of the generated project
-    base_dir = "{{cookiecutter.project_name}}"
-    print(os.getcwd())
+    base_dir = os.path.join(os.getcwd(), "{{cookiecutter.project_name}}")
 
-    # # Specify the source file path
-    # source_file = os.path.join(base_dir, 'path_to_folder/source_file.txt')
+    # Specify the source file path
+    source_file = os.path.join(base_dir, '.env.example')
 
     # # Specify the destination file path with a different name
-    # destination_file = os.path.join(base_dir, 'path_to_folder/destination_file.txt')
+    destination_file = os.path.join(base_dir, '.env')
 
-    # # Copy the file with a different name
-    # shutil.copyfile(source_file, destination_file)
+    # Copy the file with a different name
+    shutil.copyfile(source_file, destination_file)
 
 if __name__ == '__main__':
     createENV()
