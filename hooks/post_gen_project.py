ee98fa0e9a6487a1405ebb364ed17dd36aa64249
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-19 00:06:57 +0200

    Revert "refactor(cookiecutter): createEnv"
    This reverts commit e71273090345f7a93fc046d94cd650e0e1734aa2.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,18 @@
-from createEnv import createEnv
+import os
+import shutil
+
+def createENV():
+    # Get the base directory of the generated project
+    base_dir = os.path.join(os.getcwd(), "{{cookiecutter.project_name}}")
+
+    # Specify the source file path
+    source_file = os.path.join(base_dir, '.env.example')
+
+    # # Specify the destination file path with a different name
+    destination_file = os.path.join(base_dir, '.env')
+
+    # Copy the file with a different name
+    shutil.copyfile(source_file, destination_file)
 
 if __name__ == '__main__':
-    createEnv()
+    createENV()
