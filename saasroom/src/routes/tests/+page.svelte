d5dff9e8f69398d56e4045bc633f7275b5301c77
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-18 11:57:26 +0200

    feat(sr): created page for tests

---
+++ saasroom/src/routes/tests/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import FeatureFlag from '$lib/components/featureFlag.svelte';
+</script>
+
+<div class="p-4 space-y-3 flex flex-col">
+	<FeatureFlag flag="Test flag 1">
+		<div>not displaying, cause not active</div>
+	</FeatureFlag>
+	<hr class="border-b" />
+	<FeatureFlag flag="Test flag 2">
+		<div>displaying, cause active</div>
+	</FeatureFlag>
+</div>
