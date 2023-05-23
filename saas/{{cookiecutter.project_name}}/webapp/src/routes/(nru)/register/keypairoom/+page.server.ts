ca55db07650ff578018798434d111fbe34511b40
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-23 16:45:47 +0200

    feat(saas): implemented keypair generation

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/register/keypairoom/+page.server.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/register/keypairoom/+page.server.ts
@@ -1,31 +1,38 @@
-import type { Actions } from '@sveltejs/kit';
-import { devLog } from '$lib/utils/devLog';
+import { fail, type Actions } from '@sveltejs/kit';
+import { generateKeypair } from '$lib/auth/generateKeypair';
+import { userQuestionsKeys as qk } from '$lib/auth/userQuestions';
+import crypto from 'node:crypto';
+import { Collections } from '$lib/pocketbase-types';
 
 export const actions: Actions = {
 	default: async ({ locals, request }) => {
-		devLog(await request.formData());
-		// const data = Object.fromEntries(await request.formData()) as {
-		// 	email: string;
-		// 	password: string;
-		// 	passwordConfirm: string;
-		// };
+		const formData = await request.formData();
+		const email = formData.get('email') as string;
+		const HMAC = crypto.createHmac('sha256', email).digest('base64'); // <- here we should get HMAC from a server request?
+		const { seed } = await generateKeypair(email, HMAC, {
+			question1: formData.get(qk.question1) as string,
+			question2: formData.get(qk.question2) as string,
+			question3: formData.get(qk.question3) as string,
+			question4: formData.get(qk.question4) as string,
+			question5: formData.get(qk.question5) as string
+		});
 
-		// const data2: { key_derivation: string } = await createPBKDF({
-		// 	email: data.email,
-		// 	password: data.password
-		// });
+		const u = locals.pb.collection(Collections.Users);
+		const data = {
+			email,
+			password: seed,
+			passwordConfirm: seed
+		};
 
-		// const u = locals.pb.collection(Collections.Users);
+		try {
+			await u.create(data);
+			await u.authWithPassword(data.email, data.password);
+			await u.requestVerification(data.email);
 
-		// try {
-		// 	await u.create(data);
-		// 	await u.authWithPassword(data.email, data.password);
-		// 	await u.requestVerification(data.email);
-		// } catch (e) {
-		// 	console.log(e);
-		// 	return fail(400, { error: true, message: e.data.message, data: e.data.data });
-		// }
-
-		// throw redirect(303, '/my/dashboard');
+			return { seed };
+		} catch (e) {
+			console.log(e);
+			return fail(400, { error: true, message: e.data.message, data: e.data.data });
+		}
 	}
 };
