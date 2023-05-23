05a57e834941cce3ed157ad01ba90fedca03ef98
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-23 12:44:10 +0200

    fix(saas): removed projects page

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/my/projects/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutServerLoad } from '../../$types';
-import { currentUser } from '$lib/pocketbase';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-	await currentUser;
-	return {
-		projects: structuredClone(locals.pb.collection('projects'))
-	};
-};
