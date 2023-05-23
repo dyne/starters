633dc11fbce2c8dca9195f0e026112a68ea7f3a6
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-23 15:16:38 +0200

    fix(saas): updated flowbite config

--- saas/{{cookiecutter.project_name}}/webapp/tailwind.config.cjs
+++ saas/{{cookiecutter.project_name}}/webapp/tailwind.config.cjs
@@ -4,12 +4,29 @@ const config = {
 		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
 	],
 
-	theme: {
-		extend: {}
-	},
-
 	plugins: [require('flowbite/plugin')],
-	darkMode: 'class'
+
+	darkMode: 'class',
+
+	theme: {
+		extend: {
+			colors: {
+				// flowbite-svelte
+				primary: {
+					50: '#ebf5ff',
+					100: '#fff1ee',
+					200: '#ffe4de',
+					300: '#ffd5cc',
+					400: '#ffbcad',
+					500: '#fe795d',
+					600: '#ef562f',
+					700: '#eb4f27',
+					800: '#d3330a',
+					900: '#d3330a'
+				}
+			}
+		}
+	}
 };
 
 module.exports = config;
