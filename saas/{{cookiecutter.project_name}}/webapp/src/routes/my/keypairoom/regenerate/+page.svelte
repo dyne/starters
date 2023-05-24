6cf6918f7453e47a2b293eb925678106352715a1
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-24 16:28:41 +0200

    feat(saas): added keys regeneration from seed

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/my/keypairoom/regenerate/+page.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/my/keypairoom/regenerate/+page.svelte
@@ -1,12 +1,49 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import { Button, Heading, P, Textarea } from 'flowbite-svelte';
+	import { applyAction, enhance, type SubmitFunction } from '$app/forms';
+	import { regenerateKeypair, saveKeypairToLocalStorage } from '$lib/auth/generateKeypair.js';
+	import { log } from 'debug';
+	import { A, Button, Heading, P, Textarea } from 'flowbite-svelte';
+
+	export let data;
+
+	let success = false;
+
+	const onSubmit: SubmitFunction = async ({ data: formData, cancel }) => {
+		try {
+			const seed = formData.get('seed') as string;
+			const hmac = data.hmac;
+			const keypair = await regenerateKeypair(seed, hmac);
+			saveKeypairToLocalStorage(keypair);
+			success = true;
+		} catch (e) {
+			log(e, JSON.stringify(e));
+			cancel();
+		}
+
+		return async ({ result }) => {
+			await applyAction(result);
+		};
+	};
+
+	const textAreaPlaceholder =
+		'skin buyer sunset person run push elevator under debris soft surge man';
 </script>
 
-<form method="post" use:enhance class="space-y-4">
-	<Heading tag="h4">Regenerate keys</Heading>
-	<P>You've been redirected here because your private keys are missing.</P>
-	<P>Please type here your seed:</P>
-	<Textarea placeholder="sad" />
-	<Button>Regenerate keys</Button>
-</form>
+{#if !success}
+	<form method="post" use:enhance={onSubmit} class="space-y-4 p-6 flex flex-col">
+		<Heading tag="h4">Regenerate keys</Heading>
+		<P>You've been redirected here because your private keys are missing.</P>
+		<P>Please type here your seed:</P>
+		<Textarea name="seed" placeholder={textAreaPlaceholder} />
+		<Button type="submit">Regenerate keys</Button>
+		<A href="/my/keypairoom">Forgot the seed? Regenerate it</A>
+	</form>
+{:else}
+	<div class="space-y-4 p-6 flex flex-col">
+		<Heading tag="h4">Keys regenerated!</Heading>
+		<P>
+			Your keys have been regenerated. You can now go back to
+			<A href="/my/dashboard">your profile</A>.
+		</P>
+	</div>
+{/if}
