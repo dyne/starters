c78944bab16768ecd59cdeb312c5042371d89197
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-24 15:37:36 +0200

    refactor(saas): moved keys regeneration

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/regenerate-keypair/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { enhance } from '$app/forms';
-	import { Heading, P } from 'flowbite-svelte';
-</script>
-
-<form action="POST" use:enhance>
-	<Heading tag="h4">Regenerate keys</Heading>
-	<P>You've been redirected here because your private keys are missing.</P>
-</form>
---
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/my/keypairoom/regenerate/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { Button, Heading, P, Textarea } from 'flowbite-svelte';
+</script>
+
+<form method="post" use:enhance class="space-y-4">
+	<Heading tag="h4">Regenerate keys</Heading>
+	<P>You've been redirected here because your private keys are missing.</P>
+	<P>Please type here your seed:</P>
+	<Textarea placeholder="sad" />
+	<Button>Regenerate keys</Button>
+</form>
