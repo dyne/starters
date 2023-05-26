49819e3f62be995d85dc8a1078856d13527b6369
Alberto Lerda <albertolerda97@gmail.com>
2023-05-26 17:32:18 +0200

    feat: add route to create a did

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/my/(protected)/dashboard/+page.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/my/(protected)/dashboard/+page.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
 	import { appTitle } from '$lib/strings';
+	import PocketBase from 'pocketbase';
+
+	const handleClick = async () => {
+		const pb = new PocketBase('http://127.0.0.1:8090');
+		let didDiv = document.getElementById("currentDid");
+		let statusDiv = document.getElementById("didStatus");
+		const res = await pb.send("/api/did");
+		console.log(res);
+		statusDiv.innerHTML = res.created ? "CREATO" : "LETTO";
+		didDiv.innerHTML = JSON.stringify(res.did);
+	}
 </script>
 
 <h1>{appTitle} dashboard</h1>
+
+<button on:click|once={handleClick} class="border-solid  p-2 bg-gray-300">
+	Create DID
+</button>
+<div id="didStatus">
+</div>
+<div id="currentDid">
+</div>
+
