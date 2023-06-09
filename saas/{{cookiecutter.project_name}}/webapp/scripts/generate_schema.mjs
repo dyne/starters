daa0f78b8868b71e49df2d3712eb360caa62803e
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-09 09:24:44 +0200

    feat(saas:webapp): edited schema gen script

--- saas/{{cookiecutter.project_name}}/webapp/scripts/generate_schema.mjs
+++ saas/{{cookiecutter.project_name}}/webapp/scripts/generate_schema.mjs
@@ -1,15 +1,15 @@
 import PocketBase from 'pocketbase';
 import fs from 'fs';
-import 'dotenv/config'
+import 'dotenv/config';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
-const {PB_ADMIN_USER, PB_ADMIN_PASS} = process.env;
+const { PB_ADMIN_USER, PB_ADMIN_PASS, PUBLIC_POCKETBASE_URL } = process.env;
+const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 
 await pb.admins.authWithPassword(PB_ADMIN_USER, PB_ADMIN_PASS);
 
 // after the above you can also access the auth data from the authStore
 const getSchema = async () => {
-	return JSON.stringify(await pb.collections.getFullList(), null, 8)
-}
+	return JSON.stringify(await pb.collections.getFullList(), null, 4);
+};
 
-fs.writeFileSync('./src/lib/schema/pb_schema.json', await getSchema())
+fs.writeFileSync('./src/lib/schema/pb_schema.json', await getSchema());
