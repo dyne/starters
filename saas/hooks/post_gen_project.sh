7a2fdca6d3509d6dbe9b0b4d6e0c6d99dac851ba
Puria Nafisi Azizi <puria@dyne.org>
2023-06-11 10:05:46 +0200

    feat(saas): Initialize commid and setup submodules

--- saas/hooks/post_gen_project.sh
+++ saas/hooks/post_gen_project.sh
@@ -1,13 +1,21 @@
 #!/bin/env bash
 
-set -eo pipefail
+set -euo pipefail
 
 # cd ../{{cookiecutter.project_name}}/admin/pb_data
 # VALUE=$(sqlite3 data.db 'select value from _params where key like "settings";' | sed -En 's/Acme/{{cookiecutter.project_name}}/p')
 # sqlite3 data.db "update _params set value = '${VALUE}' where key like 'settings'"
 
-echo 🎉 Everything is done please run in your console
+cd {{cookiecutter.project_name}}
+git init
+git branch -m main
+rmdir admin/zencode/zenflows-crypto
+git submodule add https://github.com/interfacerproject/zenflows-crypto admin/zencode/zenflows-crypto
+
+echo 🎉 Everything is done please run
 
 echo
 
-echo docker compose up
+echo make up
+
+echo 
