fdcaf5eaefa3b6d7b94e821496bea73c7712ffd5
Puria Nafisi Azizi <puria@dyne.org>
2023-06-11 10:09:37 +0200

    fix(saas): run hooks in root dir as per
    https://cookiecutter.readthedocs.io/en/stable/advanced/hooks.html?highlight=hooks

--- saas/hooks/post_gen_project.sh
+++ saas/hooks/post_gen_project.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 # VALUE=$(sqlite3 data.db 'select value from _params where key like "settings";' | sed -En 's/Acme/{{cookiecutter.project_name}}/p')
 # sqlite3 data.db "update _params set value = '${VALUE}' where key like 'settings'"
 
-cd {{cookiecutter.project_name}}
 git init
 git branch -m main
 rmdir admin/zencode/zenflows-crypto
