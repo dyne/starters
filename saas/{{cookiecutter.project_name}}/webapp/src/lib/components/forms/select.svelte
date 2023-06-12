5fe01580ff308c8660957b7ccec96d3566a80ef2
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-12 19:22:06 +0200

    feat(webapp:saas): forms: added flowbite select

--- saas/{{cookiecutter.project_name}}/webapp/src/lib/components/forms/select.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/components/forms/select.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
 	import { formFieldProxy } from 'sveltekit-superforms/client';
 
-	import { Helper, Label } from 'flowbite-svelte';
+	import { Helper, Label, Select } from 'flowbite-svelte';
 	import { getFormContext } from './form.svelte';
+	import type { SelectOptionType } from 'flowbite-svelte/dist/types';
 
 	export let field: string;
 	export let label = '';
 	export let options: string[] = [];
+
+	const selectOptions: Array<SelectOptionType> = options.map((o) => {
+		return { value: o, name: o };
+	});
 
 	const { superform } = getFormContext();
 	const { value, errors, constraints } = formFieldProxy(superform, field);
 </script>
 
 <div class="space-y-2">
-	<Label color={$errors ? 'red' : 'gray'} for={field} class="mb-2">{label}</Label>
-	<select bind:value={$value} {...$constraints}>
-		{#each options as option}
-			<option value={option}>
-				{option}
-			</option>
-		{/each}
-	</select>
+	<Label color={$errors ? 'red' : 'gray'} for={field} class="mb-2">
+		<span>{label}</span>
+		<Select items={selectOptions} bind:value={$value} />
+	</Label>
 	{#if $errors}
 		<Helper class="mt-2" color="red">{$errors}</Helper>
 	{/if}
