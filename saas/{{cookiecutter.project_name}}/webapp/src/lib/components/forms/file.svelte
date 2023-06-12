20154a5ae2c179f149674a50d545090a1640534c
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-12 13:23:32 +0200

    fix(saas:webapp): temp: removed broken error display on File

--- saas/{{cookiecutter.project_name}}/webapp/src/lib/components/forms/file.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/components/forms/file.svelte
@@ -3,6 +3,7 @@
 
 	import { Helper, Label } from 'flowbite-svelte';
 	import { getFormContext } from './form.svelte';
+	import { log } from 'debug';
 
 	export let field: string;
 	export let label = '';
@@ -13,10 +14,13 @@
 
 	let files: FileList | undefined;
 	$: if (files) $value = [...files];
+
+	$: log($errors);
 </script>
 
 <div class="space-y-2">
-	<Label color={$errors ? 'red' : 'gray'} for={field} class="mb-2">{label}</Label>
+	<Label color={'gray'} for={field} class="mb-2">{label}</Label>
+	<!-- <Label color={$errors ? 'red' : 'gray'} for={field} class="mb-2">{label}</Label> -->
 	<input
 		name={field}
 		data-invalid={$errors}
@@ -25,7 +29,7 @@
 		{...$formConstraints}
 		{...constraints}
 	/>
-	{#if $errors}
-		<Helper class="mt-2" color="red">ds</Helper>
-	{/if}
+	<!-- {#if $errors}
+		<Helper class="mt-2" color="red">{JSON.stringify($errors)}</Helper>
+	{/if} -->
 </div>
