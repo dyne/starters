eaf9ed7397324839687937118b884509bc62d225
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-16 17:39:56 +0200

    feat(saas:webapp): sending users away from 'my' if no auth feature

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/my/+layout.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/my/+layout.ts
@@ -1,7 +1,12 @@
+import { featuresNames, isFeatureActive, loadFeatures } from '$lib/features';
 import { pb } from '$lib/pocketbase';
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 export const load = async () => {
+	if (!isFeatureActive(await loadFeatures(), featuresNames.AUTH)) {
+		throw error(404, 'Not found');
+	}
+
 	if (!pb.authStore.model) {
 		throw redirect(303, '/login');
 	}
