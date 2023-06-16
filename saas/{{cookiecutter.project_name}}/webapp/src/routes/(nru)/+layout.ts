5ed7975b4fd3012a22c388c7133ddead853024f6
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-16 17:38:59 +0200

    feat(saas:webapp): sending away users from (nru) if no auth

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/+layout.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/+layout.ts
@@ -1,7 +1,11 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import { pb } from '$lib/pocketbase';
+import { featuresNames, isFeatureActive, loadFeatures } from '$lib/features';
 
 export const load = async () => {
+	if (!isFeatureActive(await loadFeatures(), featuresNames.AUTH)) {
+		throw error(404, 'Not found');
+	}
 	if (pb.authStore.model) {
 		throw redirect(303, '/my');
 	}
