78bc3373d90fd4bb5233d66d0ed01f0329f86a2a
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-20 13:17:14 +0200

    test(saas:webapp): cell customization demo

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/tests/crud-table/+page.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/tests/crud-table/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import { Collections } from '$lib/pocketbase-types';
 	import CrudTable from '$lib/schema/CRUDTable.svelte';
+	import Chip from '$lib/components/table/cells/chip.svelte';
+	import File from '$lib/components/table/cells/file.svelte';
 </script>
 
 <div class="p-4">
 	<CrudTable
 		collection={Collections.CrudExample}
-		displayFields={['id', 'text', 'relation_single', 'relation']}
+		fields={['id', 'image', 'text', 'relation_single', 'relation']}
+		fieldsDisplay={{
+			text: Chip,
+			image: File
+		}}
 		actions={[
 			{
 				name: 'ciao',
