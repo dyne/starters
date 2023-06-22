74dbbd6ed324376b393f3e2687d0ee18587ca9b9
Puria Nafisi Azizi <puria@dyne.org>
2023-06-22 02:42:49 +0200

    fix(saas): crud updates

--- saas/hooks/post_gen_project.sh
+++ saas/hooks/post_gen_project.sh
@@ -1,21 +1,19 @@
 #!/bin/env bash
-
 set -euo pipefail
 
-# cd ../{{cookiecutter.project_name}}/admin/pb_data
-# VALUE=$(sqlite3 data.db 'select value from _params where key like "settings";' | sed -En 's/Acme/{{cookiecutter.project_name}}/p')
-# sqlite3 data.db "update _params set value = '${VALUE}' where key like 'settings'"
+# if [[ ! -d .git ]]; then
+#	git init
+#	git branch -m main
+#	git add .
+# fi
 
-if [ ! -d .git ]; then
-	git init
-	git branch -m main
-fi
-
-ZENCODE_CRYPTO=admin/zencode/zenflows-crypto
-if [[ -z $(ls -A $ZENCODE_CRYPTO) ]]; then
-	rmdir $ZENCODE_CRYPTO || true
-	git submodule add https://github.com/interfacerproject/zenflows-crypto $ZENCODE_CRYPTO || true
-fi
+# ZENCODE_CRYPTO=admin/zencode/zenflows-crypto
+# if [[ -z $(ls -A $ZENCODE_CRYPTO) ]]; then
+# 	rmdir $ZENCODE_CRYPTO || true
+#	git submodule add https://github.com/interfacerproject/zenflows-crypto $ZENCODE_CRYPTO || true
+# fi
+#
+[ ! -f webapp/.env ] && cp webapp/.env.example webapp/.env
 
 echo 🎉 Everything is done please run
 
