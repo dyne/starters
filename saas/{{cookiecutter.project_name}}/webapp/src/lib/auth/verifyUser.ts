09bcdafba434efdcc33a238f5a3f5608f34a8750
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-29 16:31:40 +0200

    fix(saas): updated verifyUser

--- saas/{{cookiecutter.project_name}}/webapp/src/lib/auth/verifyUser.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/auth/verifyUser.ts
@@ -3,15 +3,17 @@ import { pb } from '$lib/pocketbase';
 import { Collections } from '$lib/pocketbase-types';
 
 // Reference: https://github.com/pocketbase/js-sdk/issues/85
+
 export async function verifyUser(): Promise<boolean> {
 	if (!browser) throw new Error('verifyUser() must be called from the browser');
+	if (!document || !document.cookie) return false;
 
 	pb.authStore.loadFromCookie(document.cookie);
 	try {
-		pb.authStore.isValid && (await pb.collection(Collections.Users).authRefresh());
-		return true;
+		await pb.collection(Collections.Users).authRefresh();
 	} catch (_) {
 		pb.authStore.clear();
 		return false;
 	}
+	return true;
 }
