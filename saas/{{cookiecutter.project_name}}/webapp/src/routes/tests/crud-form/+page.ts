204607adcf585ed9664e5799114071605078bff3
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-29 11:11:09 +0200

    test(saas): update

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/tests/crud-form/+page.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/tests/crud-form/+page.ts
@@ -2,6 +2,6 @@ import { pb } from '$lib/pocketbase';
 import { Collections } from '$lib/pocketbase-types';
 
 export const load = async () => {
-	const item = await pb.collection(Collections.CrudExample).getOne('i13vksnjqhcvrew');
+	const item = await pb.collection(Collections.CrudExample).getOne('j8kzhs069lbbznb');
 	return { item };
 };
