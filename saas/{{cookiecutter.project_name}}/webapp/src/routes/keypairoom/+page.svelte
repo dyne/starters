d06b9fa28b6eaeff215a449703d7b1f582d05488
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-06-30 11:03:52 +0200

    feat(saas): keypairoom: now works without logged user

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/keypairoom/+page.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/keypairoom/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-	import { userQuestions, type UserAnswers } from '$lib/keypairoom/userQuestions.js';
+	import {
+		userQuestions,
+		type UserAnswers,
+		userAnswersSchema
+	} from '$lib/keypairoom/userQuestions.js';
 	import { generateKeypair, getHMAC, saveKeyringToLocalStorage } from '$lib/keypairoom/keypair';
 	import {
 		getPublicKeysFromKeypair,
@@ -7,7 +11,7 @@
 	} from '$lib/keypairoom/updateUserPublicKeys';
 	import { currentUser, pb } from '$lib/pocketbase';
 	import { z } from 'zod';
-	import { features, featuresNames, isFeatureActive } from '$lib/features';
+	import { areFeaturesActive, features, featuresNames, isFeatureActive } from '$lib/features';
 
 	// Components
 	import { Alert, Button, Heading, Hr, P } from 'flowbite-svelte';
@@ -19,83 +23,87 @@
 
 	let seed = '';
 
-	const schema = (
-		z.object({
-			question1: z.string(),
-			question2: z.string(),
-			question3: z.string(),
-			question4: z.string(),
-			question5: z.string()
-		}) satisfies z.ZodType<UserAnswers>
-	)
-		.partial()
-		.refine((v) => {
-			return Object.values(v).filter((v) => Boolean(v)).length >= 3;
-		}, 'AT_LEAST_THREE_QUESTIONS');
+	const schema = z.object({
+		email: z.string().email(),
+		questions: userAnswersSchema
+	});
 
 	const superform = createForm(schema, async ({ form }) => {
-		const { data: formData } = form;
-		const userEmail = $currentUser?.email as string;
-		const userAnswers: UserAnswers = {
-			question1: formData.question1!,
-			question2: formData.question2!,
-			question3: formData.question3!,
-			question4: formData.question4!,
-			question5: formData.question5!
-		};
+		let { data } = form;
+		let { email } = data;
 
-		const HMAC = await getHMAC(userEmail);
-		const keypair = await generateKeypair(userEmail, HMAC, userAnswers);
+		const HMAC = await getHMAC(email);
 
+		// non-answered questions *must* be set to null
+		for (let [key, value] of Object.entries(data.questions)) {
+			if (!value) data.questions[key] = 'null';
+		}
+
+		const keypair = await generateKeypair(email, HMAC, data.questions);
 		saveKeyringToLocalStorage(keypair.keyring);
 		seed = keypair.seed;
 
-		const publicKeys = getPublicKeysFromKeypair(keypair);
-		await updateUserPublicKeys($currentUser?.id!, publicKeys);
+		if (isFeatureActive($features, featuresNames.AUTH) && $currentUser) {
+			const publicKeys = getPublicKeysFromKeypair(keypair);
+			await updateUserPublicKeys($currentUser?.id!, publicKeys);
+		}
 
-		if (isFeatureActive($features, featuresNames.DID)) await pb.send('/api/did', {});
+		if (areFeaturesActive($features, [featuresNames.DID, featuresNames.AUTH]) && $currentUser) {
+			await pb.send('/api/did', {});
+		}
 	});
 
-	const { capture, restore } = superform;
+	const { capture, restore, form } = superform;
 	export const snapshot = { capture, restore };
+
+	if ($currentUser) $form.email = $currentUser.email;
 </script>
 
 <div
 	class=" bg-white mx-auto max-w-md p-6 rounded-md shadow-md dark:bg-gray-800 dark:text-white space-y-6"
 >
 	{#if !seed}
+		<h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Generate your keys</h3>
+
+		<Alert dismissable={false} accent={false}>
+			<span slot="icon"
+				><svg
+					aria-hidden="true"
+					class="w-5 h-5"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</span>
+			<span class="sr-only">Info</span>
+			<span class="font-bold">Important information</span>
+			<ul slot="extra" class="mt-0 ml-8 list-disc pl-4 space-y-1 pt-1">
+				<li>
+					By answering these questions, you will generate keys that will be used to encrypt your
+					data.
+				</li>
+				<li>Please remember the answers, as they will be the only way to restore the keys.</li>
+				<li>Please answer at least 3 of the following questions.</li>
+			</ul>
+		</Alert>
+
 		<Form {superform} className="space-y-6">
-			<h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Generate your keys</h3>
-
-			<Alert dismissable={false} accent={false}>
-				<span slot="icon"
-					><svg
-						aria-hidden="true"
-						class="w-5 h-5"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-							clip-rule="evenodd"
-						/></svg
-					>
-				</span>
-				<span class="sr-only">Info</span>
-				<span class="font-bold">Important information</span>
-				<ul slot="extra" class="mt-0 ml-8 list-disc pl-4 space-y-1 pt-1">
-					<li>
-						By answering these questions, you will generate keys that will be used to encrypt your
-						data.
-					</li>
-					<li>Please remember the answers, as they will be the only way to restore the keys.</li>
-					<li>Please answer at least 3 of the following questions.</li>
-				</ul>
-			</Alert>
-
+			{#if !$currentUser}
+				<div class="space-y-1">
+					<Input field="email" label="User email" />
+					<P size="sm" color="text-gray-400">
+						Your email won't be stored anywhere, it will be used only to generate the keys.
+					</P>
+				</div>
+				<Hr />
+			{/if}
 			{#each userQuestions as question}
-				<Input field={question.id} label={question.text} />
+				<Input field={`questions.${question.id}`} label={question.text} />
 			{/each}
 
 			<Hr />
@@ -114,6 +122,6 @@
 				</div>
 			</span>
 		</Alert>
-		<Button href="/my/dashboard">Go to Dashboard</Button>
+		<Button href="/my">Go to Dashboard</Button>
 	{/if}
 </div>
