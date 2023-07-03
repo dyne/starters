b3663d8178cacd6fbdf542e5e4c7b830c6d61bc3
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-07-03 15:16:47 +0200

    refactor(saas): moved field components

--- saas/{{cookiecutter.project_name}}/webapp/src/lib/schema/recordsManager/views/fieldsComponents/cells/chip.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/schema/recordsManager/views/fieldsComponents/cells/chip.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	export let value = '';
+	import type { Record } from 'pocketbase';
+
+	export let value: unknown;
+	export let record: Record;
 </script>
 
 <div class="bg-gray-200 w-fit py-1 px-2 rounded-full text-xs">
