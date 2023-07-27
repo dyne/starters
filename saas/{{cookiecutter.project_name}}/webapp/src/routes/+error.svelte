9bfc12d3ed590f7c9fe1a42fc3c9e952cb141a4f
phoebus-84 <ennio@dyne.org>
2023-07-28 00:58:30 +0200

    chore: add page for errors handling

---
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/+error.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { A, Heading, P } from 'flowbite-svelte';
+	type ErrorData = {
+		status: number;
+		image: string;
+		title: string;
+		description: string;
+	};
+	const status = $page.status;
+	const data: ErrorData = (() => {
+		switch (status) {
+			case 404:
+				return {
+					status: 404,
+					image: '/404-computer.svg',
+					title: 'Not Found',
+					description: 'Whoops! That page doesn’t exist.'
+				};
+			default:
+				return {
+					status: 500,
+					image: '/500.svg',
+					title: 'Internal Error',
+					description: 'Something went wrong. Please try again later.'
+				};
+		}
+	})();
+</script>
+
+<div
+	class="flex flex-col gap-4 lg:flex-row max-w-4xl mx-auto min-h-screen pt-20 lg:pt-0 p-8 lg:p-0"
+>
+	<div class="flex flex-col items-center justify-center w-full">
+		<img src={data.image} alt={data.title} />
+	</div>
+	<div class="flex flex-col items-center justify-center w-full gap-4">
+		<Heading tag="h3" class="text-primary-600">{data.status} {data.title}</Heading>
+		<Heading tag="h2">{data.description}</Heading>
+		<div class="w-full pt-8">
+			<P class="text-gray-400">Here are some Helpful link:</P>
+			<ul class="flex gap-2">
+				<li><A href="/">Home</A></li>
+				<li><A href="/login">Login</A></li>
+				<li><A href="/register">Register</A></li>
+				<li><A href="/my">My</A></li>
+			</ul>
+		</div>
+	</div>
+</div>
