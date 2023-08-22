9db8e594275e3aa4bbf2a2794c2fb800a42765c6
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-08-22 13:13:19 +0200

    feat(saas: webauthn): added section displaying methods

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/my/profile/_partials/userWebauthn.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/my/profile/_partials/userWebauthn.svelte
@@ -2,17 +2,30 @@
 	import { registerUser, isWebauthnSupported } from '$lib/webauthn';
 	import { currentUser } from '$lib/pocketbase';
 
-	import { Alert, Button, Heading, P } from 'flowbite-svelte';
+	import { Alert, Button, Card, Heading, P } from 'flowbite-svelte';
 	import { InformationCircle, Plus } from 'svelte-heros-v2';
-	// import { onMount } from 'svelte';
-
-	// onMount(async () => {
-	// 	console.log(await navigator.credentials.get());
-	// });
+	import RecordsManager from '$lib/schema/recordsManager/recordsManager.svelte';
+	import { Collections } from '$lib/pocketbase-types';
+	import DeleteRecord from '$lib/schema/recordsManager/recordActions/deleteRecord.svelte';
 </script>
 
 <Heading tag="h6">Your devices</Heading>
 <P color="gray" size="sm">Manage the devices you use to login.</P>
+
+<RecordsManager collection={Collections.WebauthnCredentials} let:records>
+	<div class="space-y-2 py-4">
+		{#each records as record}
+			<Card class="max-w-none">
+				<div class="flex justify-between gap-4 items-center">
+					<div class="w-0 grow overflow-hidden">
+						<P>{record.credential.ID}</P>
+					</div>
+					<DeleteRecord {record} />
+				</div>
+			</Card>
+		{/each}
+	</div>
+</RecordsManager>
 
 {#if !isWebauthnSupported()}
 	<Alert color="red" class="mt-2">
