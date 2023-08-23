9e28dc6f5744f968119817719d09548d816f33a8
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-08-23 11:12:08 +0200

    fix(saas:login): add feature flag tag

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/login/+layout.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/login/+layout.svelte
@@ -5,8 +5,10 @@
 
 <script lang="ts">
 	import { page } from '$app/stores';
+	import FeatureFlag from '$lib/components/featureFlag.svelte';
 	import type { Link } from '$lib/utils/types';
-	import { Button, ButtonGroup, Heading } from 'flowbite-svelte';
+	import { Button, ButtonGroup, Heading, P } from 'flowbite-svelte';
+	import { featuresNames } from '$lib/features';
 
 	const modes: Link[] = [
 		{
@@ -21,12 +23,17 @@
 </script>
 
 <Heading tag="h4">Log in</Heading>
-<ButtonGroup class="w-full">
-	{#each modes as { href, text }}
-		{@const isActive = $page.url.pathname === href}
-		<Button color={isActive ? 'dark' : 'alternative'} {href} class="grow">{text}</Button>
-	{/each}
-</ButtonGroup>
+<FeatureFlag flag={featuresNames.WEBAUTHN}>
+	<div class="space-y-2">
+		<P size="sm" color="text-gray-500">Choose your authentication method</P>
+		<ButtonGroup class="w-full">
+			{#each modes as { href, text }}
+				{@const isActive = $page.url.pathname === href}
+				<Button color={isActive ? 'dark' : 'alternative'} {href} class="grow">{text}</Button>
+			{/each}
+		</ButtonGroup>
+	</div>
+</FeatureFlag>
 
 <div class="pt-4">
 	<slot />
