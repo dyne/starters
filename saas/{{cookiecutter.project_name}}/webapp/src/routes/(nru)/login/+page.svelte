6cc45aac54df0439ca1c28aa43ca32ae2273eb83
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-08-23 10:40:55 +0200

    feat(saas:login): saving email between login modes

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/login/+page.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/login/+page.svelte
@@ -2,32 +2,38 @@
 	import { goto } from '$app/navigation';
 	import { pb } from '$lib/pocketbase';
 	import { Collections } from '$lib/pocketbase-types';
-	import { Heading } from 'flowbite-svelte';
 	import Form, { createForm } from '$lib/components/forms/form.svelte';
 	import Input from '$lib/components/forms/input.svelte';
 	import { z } from 'zod';
 	import FormError from '$lib/components/forms/formError.svelte';
 	import SubmitButton from '$lib/components/forms/submitButton.svelte';
+	import { currentEmail } from './+layout.svelte';
 
 	const schema = z.object({
 		email: z.string().email(),
 		password: z.string()
 	});
 
-	const superform = createForm(schema, async ({ form }) => {
-		const { data } = form;
-		const u = pb.collection(Collections.Users);
-		await u.authWithPassword(data.email, data.password);
-		await goto('/my');
-	});
+	const superform = createForm(
+		schema,
+		async ({ form }) => {
+			const { data } = form;
+			const u = pb.collection(Collections.Users);
+			await u.authWithPassword(data.email, data.password);
+			await goto('/my');
+		},
+		{ email: $currentEmail },
+		{ taintedMessage: null }
+	);
 	const keys = schema.keyof().Enum;
 
-	const { capture, restore } = superform;
+	const { capture, restore, form } = superform;
 	export const snapshot = { capture, restore };
+
+	$: $currentEmail = $form.email;
 </script>
 
 <Form {superform}>
-	<Heading tag="h4">Log in</Heading>
 	<Input
 		id="email"
 		type="email"
--- saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/login/webauthn/+page.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/(nru)/login/webauthn/+page.svelte
@@ -8,24 +8,31 @@
 	import Input from '$lib/components/forms/input.svelte';
 	import FormError from '$lib/components/forms/formError.svelte';
 	import SubmitButton from '$lib/components/forms/submitButton.svelte';
+	import { currentEmail } from '../+layout.svelte';
 
 	const schema = z.object({
 		email: z.string().email()
 	});
 
-	const superform = createForm(schema, async ({ form }) => {
-		const { data } = form;
-		await loginUser(data.email);
-		await goto('/my');
-	});
+	const superform = createForm(
+		schema,
+		async ({ form }) => {
+			const { data } = form;
+			await loginUser(data.email);
+			await goto('/my');
+		},
+		{ email: $currentEmail },
+		{ taintedMessage: null }
+	);
 	const keys = schema.keyof().Enum;
 
-	const { capture, restore } = superform;
+	const { capture, restore, form } = superform;
 	export const snapshot = { capture, restore };
+
+	$: $currentEmail = $form.email;
 </script>
 
 <Form {superform}>
-	<Heading tag="h4">Log in</Heading>
 	<Input
 		id="email"
 		type="email"
