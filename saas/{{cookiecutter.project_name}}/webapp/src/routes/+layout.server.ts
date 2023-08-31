302802163d6f90c57e6cc5094c21202d752cd076
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-08-31 12:33:29 +0200

    Update +layout.server.ts

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/+layout.server.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/+layout.server.ts
@@ -1,13 +1,11 @@
-import { MAINTENANCE } from '$env/static/private';
-import { features, loadFeatures } from '$lib/features';
+import { loadFeatureFlags } from '$lib/features';
 import { error } from '@sveltejs/kit';
 
 export const ssr = false;
 
 export const load = async () => {
-	const feats = await loadFeatures();
-	if (feats.find((f) => f.name === 'maintenance')?.active || MAINTENANCE === 'true') {
+	const flags = await loadFeatureFlags();
+	if (flags.MAINTENANCE) {
 		throw error(503);
 	}
-	features.set(feats);
 };
