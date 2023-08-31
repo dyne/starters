f65c1d64910a214026954016ca52a95540887543
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2023-08-31 15:05:19 +0200

    style(saas): skeleton placeholders for crud table and cards (#143)
    - fix: modal wrapper z-index
    - style: table skeleton loading
    - style: cards skeleton loading
    
    ---------
    
    Co-authored-by: phoebus-84 <ennio@dyne.org>

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/tests/records-manager/+page.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/tests/records-manager/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import GridSpinner from '$lib/components/gridSpinner.svelte';
+	import TableSkeleton from '$lib/components/tableSkeleton.svelte';
 	import { currentUser } from '$lib/pocketbase';
 	import { Collections, type CrudExampleRecord } from '$lib/pocketbase-types';
 	import FilterRecords from '$lib/schema/recordsManager/filterRecords.svelte';
@@ -10,7 +12,7 @@
 	import Chip from '$lib/schema/recordsManager/views/fieldsComponents/cells/chip.svelte';
 	import RecordCard from '$lib/schema/recordsManager/views/recordCard.svelte';
 	import RecordsTable from '$lib/schema/recordsManager/views/recordsTable.svelte';
-	import { Heading, Hr } from 'flowbite-svelte';
+	import { CardPlaceholder, Heading, Hr, Skeleton } from 'flowbite-svelte';
 	import { XCircle } from 'svelte-heros-v2';
 
 	const slotTypeCaster = createSlotTypeCaster<CrudExampleRecord>();
@@ -28,6 +30,7 @@
 		}}
 		{slotTypeCaster}
 		let:records
+		let:loadRecords
 	>
 		<div class="space-y-8">
 			<RecordsManagerTopbar />
@@ -36,40 +39,58 @@
 
 			<div class="space-y-4">
 				<Heading tag="h4">Table</Heading>
-				<RecordsTable
-					{records}
-					fields={['id', 'text', 'textarea']}
-					emptyState={{
-						title: 'No records',
-						description: 'There are no records to show.'
-					}}
-				/>
-				<!-- add this component where you like, within recordsManager and indicate which fields to search for -->
-				<FilterRecords searchableFields={['text', 'textarea']} />
-				<RecordsTable {records} fields={['id', 'text', 'textarea']} />
+				{#await loadRecords()}
+					<TableSkeleton columns={['checkbox', 'image', 'short text', 'long text', 'actions']} />
+				{:then}
+					{#if records.length === 0}
+						<EmptyState title={'No records'} description={'Start adding records.'} icon={XCircle} />
+					{:else}
+						<FilterRecords searchableFields={['text', 'textarea']} />
+						<RecordsTable
+							{records}
+							fields={['id', 'text', 'textarea']}
+							emptyState={{
+								title: 'No records',
+								description: 'There are no records to show.'
+							}}
+						/>
+					{/if}
+				{:catch}
+					<EmptyState title={'Error'} description={'Something went wrong.'} icon={XCircle} />
+				{/await}
 			</div>
 
 			<Hr />
 
 			<div class="space-y-4">
 				<Heading tag="h4">Cards</Heading>
-				{#if records.length === 0}
-					<EmptyState title={'No records'} description={'Start adding records.'} icon={XCircle}/>
-				{:else}
+
+				{#await loadRecords()}
 					<div class="grid grid-cols-4 gap-4">
-						{#each records as record}
-							<RecordCard
-								{record}
-								titleField="id"
-								fields={['text', 'select', 'textarea']}
-								fieldsComponents={{ select: Chip }}
-								showEdit
-								showCheckbox
-								showDelete
-							/>
+						{#each Array(4) as _}
+							<CardPlaceholder />
 						{/each}
 					</div>
-				{/if}
+				{:then}
+					{#if records.length === 0}
+						<EmptyState title={'No records'} description={'Start adding records.'} icon={XCircle} />
+					{:else}
+						<div class="grid grid-cols-4 gap-4">
+							{#each records as record}
+								<RecordCard
+									{record}
+									titleField="id"
+									fieldsComponents={{ select: Chip }}
+									showEdit
+									showCheckbox
+									showDelete
+								/>
+							{/each}
+						</div>
+					{/if}
+				{:catch}
+					<EmptyState title={'Error'} description={'Something went wrong.'} icon={XCircle} />
+				{/await}
 			</div>
 		</div>
 	</RecordsManager>
