97831029dfbb7e955f16e0d02b47327b21a06a8e
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-09-15 22:16:24 +0200

    feat(saas): record form: custom field labels (#195)

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/tests/collection-manager/+page.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/tests/collection-manager/+page.svelte
@@ -24,7 +24,12 @@
 		{recordType}
 		collection={Collections.CrudExample}
 		formSettings={{
-			hide: { owner: $currentUser?.id }
+			hide: { owner: $currentUser?.id },
+			labels: {
+				text: 'Titolo',
+				file_only_pdf_json: 'PDF or JSON file',
+				textarea: 'Short description'
+			}
 		}}
 		editFormSettings={{
 			exclude: ['select']
