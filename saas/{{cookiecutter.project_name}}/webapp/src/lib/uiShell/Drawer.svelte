626c7989dd1348cb5dd7ce34425c2fb7536887b7
phoebus-84 <ennio@dyne.org>
2023-09-19 09:59:18 +0200

    refactor: the drawer component can also come from the right

--- saas/{{cookiecutter.project_name}}/webapp/src/lib/uiShell/Drawer.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/uiShell/Drawer.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { CloseButton, Drawer } from 'flowbite-svelte';
 	import { sineIn } from 'svelte/easing';
 	import { getUiShellContext } from './UiShell.svelte';
@@ -6,8 +6,12 @@
 
 	const { drawerHidden, activateClickOutside, backdrop } = getUiShellContext();
 
+	export let placement: 'left' | 'right'  = 'left';
+
+	export let divClass = `overflow-hidden z-50 p-4 bg-white w-fit fixed inset-y-0 ${placement}-0`
+
     let transitionParams = {
-		x: -320,
+		x: placement == 'right'? 320 : -320,
 		duration: 200,
 		easing: sineIn
 	};
@@ -20,9 +24,10 @@
 	{transitionParams}
 	bind:hidden={$drawerHidden}
 	activateClickOutside={$activateClickOutside}
+	{placement}
 	width="w-64"
 	id="sidebar"
-	divClass="overflow-hidden z-50 p-4 bg-white dark:bg-gray-800 w-fit fixed inset-y-0 left-0"
+	{divClass}
 >
 	<div class="flex items-center mb-2 px-3">
 		<Logo/>
