aa608013d472a2bf23fca97bd79b84da5e73c23a
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-09-22 13:16:03 +0200

    feat(saas): add development notes

--- saas/{{cookiecutter.project_name}}/webapp/src/hooks.client.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/hooks.client.ts
@@ -1,7 +1,9 @@
 import { currentUser, pb, type AuthStoreModel } from '$lib/pocketbase';
+import { Collections } from '$lib/pocketbase/types';
 
 pb.authStore.loadFromCookie(document.cookie);
-pb.authStore.onChange(() => {
+pb.authStore.onChange(async () => {
+	// TODO: Add all roles in all organizations for the user
 	currentUser.set(pb.authStore.model as AuthStoreModel);
 	document.cookie = pb.authStore.exportToCookie({ httpOnly: false });
 });
--- saas/{{cookiecutter.project_name}}/webapp/src/lib/pocketbase/index.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/pocketbase/index.ts
@@ -1,9 +1,10 @@
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
-import type { UsersResponse } from './types';
+import type { OrganizationRolesResponse, UsersResponse } from './types';
 
 export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 
 export const currentUser = writable(pb.authStore.model as AuthStoreModel);
 export type AuthStoreModel = UsersResponse | null;
+// TODO: Add all roles in all organizations for the user
