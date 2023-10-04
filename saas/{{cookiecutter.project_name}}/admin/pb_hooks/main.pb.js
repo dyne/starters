d1bd1f7266820a7e3f01f1273fb38d4eac5bead4
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-10-04 16:31:21 +0200

    feat(saas): create admin on organization create

--- saas/{{cookiecutter.project_name}}/admin/pb_hooks/main.pb.js
+++ saas/{{cookiecutter.project_name}}/admin/pb_hooks/main.pb.js
@@ -1,4 +1,24 @@
-// pb_hooks/test.pb.js
-$app.onRecordBeforeCreateRequest("organizationRoles").add((e) => {
-    console.log(e)
-})
+/// <reference path="../pb_data/types.d.ts" />
+
+onRecordAfterCreateRequest((e) => {
+    console.log("Hook - Creating admin role for new organization");
+
+    const userId = $apis.requestInfo(e.httpContext).authRecord.id;
+    const organizationId = e.record.id;
+
+    const adminRole = $app
+        .dao()
+        .findFirstRecordByData("organizationRoles", "name", "admin");
+    if (!adminRole) throw new Error("missing admin role!");
+    const adminRoleId = adminRole.id;
+
+    const collection = $app
+        .dao()
+        .findCollectionByNameOrId("organizationAuthorizations");
+    const record = new Record(collection, {
+        organization: organizationId,
+        role: adminRoleId,
+        user: userId,
+    });
+    $app.dao().saveRecord(record);
+}, "organizations");
