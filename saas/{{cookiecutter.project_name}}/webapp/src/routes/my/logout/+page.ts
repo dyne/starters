6486ed357ece8aa67bb5000d4e40e60e1706eabc
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-10-06 15:44:04 +0200

    fix(saas): removed unused code

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/my/logout/+page.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/my/logout/+page.ts
@@ -1,6 +1,5 @@
 import { browser } from '$app/environment';
 import { currentUser, pb } from '$lib/pocketbase';
-import { rolesStore } from '$lib/roles';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {
@@ -8,7 +7,6 @@ export const load = async () => {
 		localStorage.clear();
 		pb.authStore.clear();
 		currentUser.set(null);
-		rolesStore.set({});
 		throw redirect(303, '/');
 	}
 };
