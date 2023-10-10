f97fd69c50d9b7f0a65abda2551102b3f65f9261
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-10-10 17:58:07 +0200

    fix(saas): remove async

--- saas/{{cookiecutter.project_name}}/webapp/src/hooks.client.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/hooks.client.ts
@@ -1,7 +1,7 @@
 import { currentUser, pb, type AuthStoreModel } from '$lib/pocketbase';
 
 pb.authStore.loadFromCookie(document.cookie);
-pb.authStore.onChange(async () => {
+pb.authStore.onChange(() => {
 	currentUser.set(pb.authStore.model as AuthStoreModel);
 	document.cookie = pb.authStore.exportToCookie({ httpOnly: false });
 });
