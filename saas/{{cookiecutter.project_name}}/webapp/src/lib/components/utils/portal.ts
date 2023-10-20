9d7069d2f92ae5145a51b084c2731f38d0994a83
phoebus-84 <ennio@dyne.org>
2023-10-20 02:05:14 +0200

    feat: use-portal

---
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/components/utils/portal.ts
@@ -0,0 +1,44 @@
+import { tick } from 'svelte';
+
+export function portal(
+	el: HTMLElement,
+	target: HTMLElement | string = 'body'
+): {
+	update: (newTarget: HTMLElement | string) => Promise<void>;
+	destroy: () => void;
+} {
+	let targetEl: HTMLElement | null = null;
+
+	async function update(newTarget: HTMLElement | string): Promise<void> {
+        target = newTarget;
+		if (typeof target === 'string') {
+			targetEl = targetEl ?? document.querySelector(target);
+
+			if (!targetEl) {
+				await tick();
+				targetEl = document.querySelector(target);
+			}
+
+			if (!targetEl) throw new Error(`No element found matching CSS selector: "${target}"`);
+		} else if (target instanceof HTMLElement) {
+			targetEl = target;
+		} else {
+			throw new TypeError(
+				`Unknown portal target type: ${
+					target === null ? 'null' : typeof target
+				}. Allowed types: string (CSS selector) or HTMLElement.`
+			);
+		}
+
+		targetEl.appendChild(el);
+		el.hidden = false;
+	}
+
+	function destroy(): void {
+		if (el.parentElement) el.parentElement.removeChild(el);
+	}
+
+	update(target);
+
+    return { update, destroy };
+}
