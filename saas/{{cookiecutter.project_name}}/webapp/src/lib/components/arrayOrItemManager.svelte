3f4f5a945e2ea2354fa2536cc555b882067e5873
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-11-09 15:11:09 +0100

    feat(saas): updated listgroupItemSlotted component

--- saas/{{cookiecutter.project_name}}/webapp/src/lib/components/arrayOrItemManager.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/components/arrayOrItemManager.svelte
@@ -1,39 +1,46 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-	import { Listgroup } from 'flowbite-svelte';
-	import ListgroupItemButton from './listgroupItemButton.svelte';
+	import IconButton from './iconButton.svelte';
+	import { Listgroup, ListgroupItem } from 'flowbite-svelte';
+	import ListgroupItemSlotted from './listgroupItemSlotted.svelte';
+
+	//
 
 	type T = $$Generic;
 	export let value: T | T[] | undefined;
 
+	//
+
 	let tempItems: T[] = [];
-	$: {
-		if (Array.isArray(value)) tempItems = value;
-		else if (value) tempItems = [value];
+	$: createTempItems(value);
+
+	function createTempItems(v: typeof value) {
+		if (Array.isArray(v)) tempItems = v;
+		else if (v) tempItems = [v];
 		else tempItems = [];
 	}
-
-	const dispatch = createEventDispatcher<{
-		show: {
-			item: T;
-		};
-	}>();
 
 	function removeItem(item: T) {
 		if (Array.isArray(value)) value = value.filter((i) => i !== item);
 		else value = undefined;
-	}
-	function showItem(item: T) {
-		dispatch('show', {item} );
 	}
 </script>
 
 {#if tempItems.length > 0}
 	<Listgroup>
 		{#each tempItems as item (item)}
-			<ListgroupItemButton on:remove={() => removeItem(item)} on:show={() => showItem(item)}>
-				<slot {item} />
-			</ListgroupItemButton>
+			<ListgroupItemSlotted>
+				<svelte:fragment slot="left">
+					<slot {item} />
+				</svelte:fragment>
+				<svelte:fragment slot="right">
+					<slot name="right" {item} />
+					<IconButton
+						on:click={() => {
+							removeItem(item);
+						}}
+					/>
+				</svelte:fragment>
+			</ListgroupItemSlotted>
 		{/each}
 	</Listgroup>
 {/if}
