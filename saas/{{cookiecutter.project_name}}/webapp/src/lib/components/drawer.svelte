7f5328db45c2c03cf08faa70ffb5ed850517a2f2
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-11-09 15:39:01 +0100

    refactor(saas): created drawer component

---
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/components/drawer.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Drawer } from 'flowbite-svelte';
+	import { sineIn } from 'svelte/easing';
+
+	export let hidden = true;
+	export let placement: 'left' | 'right' = 'left';
+	export let width = 'w-40';
+	export let backdrop = true;
+	export let closeOnClickOutside = true;
+
+	$: transitionDirection = placement == 'right' ? 1 : -1;
+	$: transitionParams = {
+		x: 320 * transitionDirection,
+		duration: 200,
+		easing: sineIn
+	};
+</script>
+
+<Drawer
+	bind:hidden
+	transitionType="fly"
+	{backdrop}
+	{transitionParams}
+	activateClickOutside={closeOnClickOutside}
+	{placement}
+	class={`flex flex-col !p-0`}
+	{width}
+>
+	<slot />
+</Drawer>
