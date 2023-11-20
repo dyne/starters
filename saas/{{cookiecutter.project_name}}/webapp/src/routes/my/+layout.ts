60a71d5c20a87731f331ccd61e1542d7f09091a7
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-11-20 09:53:01 +0100

    feat: if keyring is null, redirect the user

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/my/+layout.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/my/+layout.ts
@@ -1,8 +1,20 @@
+import { browser } from '$app/environment';
 import { verifyUser } from '$lib/auth/verifyUser';
 import { loadFeatureFlags } from '$lib/features';
+import { getKeyringFromLocalStorage } from '$lib/keypairoom/keypair';
 import { error, redirect } from '@sveltejs/kit';
 
-export const load = async () => {
-	if (!(await loadFeatureFlags()).AUTH) throw error(404);
-	if (!(await verifyUser())) throw redirect(303, '/login');
+export const load = async ({ url }) => {
+	const features = await loadFeatureFlags();
+
+	if (!features.AUTH) throw error(404);
+	else {
+		const isUserLogged = await verifyUser();
+		if (!isUserLogged) throw redirect(303, '/login');
+	}
+
+	if (features.KEYPAIROOM && browser) {
+		const keyring = getKeyringFromLocalStorage();
+		if (!keyring) throw redirect(303, `/keypairoom?${url.searchParams.toString()}`);
+	}
 };
