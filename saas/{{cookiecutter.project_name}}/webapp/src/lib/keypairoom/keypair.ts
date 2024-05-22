0fb4ceab6c827bb88dc48806f8988c87c722a332
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-05-22 19:46:22 +0200

    fix(keypairoom): improved user challenges handling (#304)

--- saas/{{cookiecutter.project_name}}/webapp/src/lib/keypairoom/keypair.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/keypairoom/keypair.ts
@@ -1,4 +1,4 @@
-import type { UserAnswers } from './userQuestions';
+import type { UserChallenges } from './userQuestions';
 import { zencode_exec } from 'zenroom';
 import keypairoomClient from '../../../zenflows-crypto/src/keypairoomClient-8-9-10-11-12.zen?raw';
 import keypairoomClientRecreateKeys from '../../../zenflows-crypto/src/keypairoomClientRecreateKeys.zen?raw';
@@ -52,17 +52,11 @@ async function zencodeExec<T>(contract: string, props: ZenroomProps): Promise<T>
 export async function generateKeypair(
 	email: string,
 	HMAC: string,
-	answers: UserAnswers
+	userChallenges: UserChallenges
 ): Promise<Keypair> {
 	return await zencodeExec<Keypair>(keypairoomClient, {
 		data: {
-			userChallenges: {
-				whereParentsMet: answers.question1,
-				nameFirstPet: answers.question2,
-				nameFirstTeacher: answers.question3,
-				whereHomeTown: answers.question4,
-				nameMotherMaid: answers.question5
-			},
+			userChallenges,
 			username: email,
 			'seedServerSideShard.HMAC': HMAC
 		}
