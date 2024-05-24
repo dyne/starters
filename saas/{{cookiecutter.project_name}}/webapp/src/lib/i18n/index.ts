8127d0c5aeef6d8618230ecfdefe458a1c21db99
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-05-24 12:07:24 +0200

    feat: exporting m from i18n

--- saas/{{cookiecutter.project_name}}/webapp/src/lib/i18n/index.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/i18n/index.ts
@@ -1,5 +1,6 @@
-// file initialized by the Paraglide-SvelteKit CLI - Feel free to edit it
-import { createI18n } from "@inlang/paraglide-sveltekit"
-import * as runtime from "$lib/paraglide/runtime.js"
+import { createI18n } from '@inlang/paraglide-sveltekit';
+import * as runtime from '$lib/paraglide/runtime.js';
+import * as m from '$lib/paraglide/messages.js';
 
-export const i18n = createI18n(runtime)
+export const i18n = createI18n(runtime);
+export { m };
