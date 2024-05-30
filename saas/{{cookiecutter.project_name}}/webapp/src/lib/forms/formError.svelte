3aa077551a1f98e8c8d3ac3e4b789de2cc083c99
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-05-30 10:32:18 +0200

    fix: porting changes from signroom

--- saas/{{cookiecutter.project_name}}/webapp/src/lib/forms/formError.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/lib/forms/formError.svelte
@@ -1,16 +1,34 @@
 <script lang="ts">
 	import { Alert } from 'flowbite-svelte';
 	import { getFormContext } from './form.svelte';
+	import { m } from '$lib/i18n';
 
 	const { superform } = getFormContext();
-	const { message } = superform;
+	const { message, allErrors } = superform;
+
+	$: error = $allErrors.at(0);
 </script>
+
+{#if error}
+	<Alert color="red" dismissable>
+		<p class="font-bold">{m.Error()}</p>
+		{#if error.messages.length > 0}
+			<ul class="mt-1 space-y-2">
+				{#each error.messages as message}
+					<li class="leading-4">
+						{message}
+					</li>
+				{/each}
+			</ul>
+		{/if}
+	</Alert>
+{/if}
 
 {#if $message}
 	<Alert color="red" dismissable>
 		<p>{$message.message}</p>
 		{#if $message.data && Object.keys($message.data).length > 0}
-			<ul class="space-y-2 mt-2">
+			<ul class="mt-2 space-y-2">
 				{#each Object.entries($message.data) as [key, value]}
 					<li class="leading-4">
 						<span class="font-bold">{key}</span><br />{value.message}
