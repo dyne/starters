fb20ea3cff1fc965c9d0ac36dc30a42d9b540d80
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-06-04 16:10:50 +0200

    fix: improve feature flag usage

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/[[lang]]/keypairoom/+layout.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/[[lang]]/keypairoom/+layout.svelte
@@ -5,23 +5,30 @@
 	import UserAvatar from '$lib/components/userAvatar.svelte';
 	import { Button } from 'flowbite-svelte';
 	import { m } from '$lib/i18n';
+	import { featureFlags } from '$lib/features';
 </script>
 
 <Topbar>
 	<Logo slot="left" />
 	<div class="flex items-center" slot="center">
-		<div>
-			<span class="whitespace-nowrap">
-				Hello, <span class="font-semibold">{$currentUser?.email}</span>
-			</span>
-		</div>
-		<div class="shrink-0">
-			<DIDButton />
-		</div>
+		{#if $featureFlags.AUTH}
+			<div>
+				<span class="whitespace-nowrap">
+					Hello, <span class="font-semibold">{$currentUser?.email}</span>
+				</span>
+			</div>
+			<div class="shrink-0">
+				<DIDButton />
+			</div>
+		{:else}
+			<div>Hello!</div>
+		{/if}
 	</div>
 	<div slot="right" class="flex items-center gap-4">
-		<Button href="/logout" size="xs" color="alternative">{m.Sign_out()}</Button>
-		<UserAvatar />
+		{#if $featureFlags.AUTH}
+			<Button href="/logout" size="xs" color="alternative">{m.Sign_out()}</Button>
+			<UserAvatar />
+		{/if}
 	</div>
 </Topbar>
 <div class="mx-auto max-w-md p-6">
