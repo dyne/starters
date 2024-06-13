a43b28c78901de5c59edf22fc08a7395014b87af
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-06-13 09:21:44 +0200

    style: format

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/+layout.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/+layout.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
-	import { i18n } from '$lib/i18n'
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
 
 	import '../app.postcss';
 	import { version } from '$app/environment';
 	console.info(`SAAS version: 🔖 ${version}`);
 </script>
+
 <ParaglideJS {i18n}>
-
-
-<slot />
-
+	<slot />
 </ParaglideJS>
