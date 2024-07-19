d3c95b130337900db47c4475f27eee1b1710fb2e
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-07-19 02:36:21 +0200

    feat: created modules/forms

---
+++ saas/{{cookiecutter.project_name}}/webapp/src/modules/forms/@old/fields/input.svelte
@@ -0,0 +1,40 @@
+<script lang="ts" context="module">
+	import { Input } from 'flowbite-svelte';
+	import type { HelpTextOption, LabelOption } from './types';
+	import type { ComponentProps } from 'svelte';
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	export type FormInputOptions = Partial<HTMLInputAttributes & ComponentProps<Input>> &
+		LabelOption &
+		HelpTextOption;
+</script>
+
+<script lang="ts">
+	import type { z } from 'zod';
+	import type { FormPathLeaves, ZodValidation } from 'sveltekit-superforms';
+	import { formFieldProxy, type SuperForm } from 'sveltekit-superforms/client';
+	import FieldWrapper from './fieldParts/fieldWrapper.svelte';
+
+	type T = $$Generic<AnyZodObject>;
+
+	export let superform: SuperForm<ZodValidation<T>, any>;
+	export let field: FormPathLeaves<z.infer<T>>;
+	export let options: FormInputOptions = {};
+
+	let type = options.type ?? 'text';
+
+	const { value, errors, constraints } = formFieldProxy(superform, field as string);
+</script>
+
+<FieldWrapper {field} label={options.label} helpText={options.helpText}>
+	<slot slot="labelRight" name="labelRight" />
+	<Input
+		{...options}
+		{type}
+		color={$errors ? 'red' : 'base'}
+		name={field}
+		data-invalid={$errors}
+		bind:value={$value}
+		{...$constraints}
+	/>
+</FieldWrapper>
---
+++ saas/{{cookiecutter.project_name}}/webapp/src/modules/forms/@old/index.ts
@@ -0,0 +1,7 @@
+import Form, { createForm, createFormData, type SubmitFunction } from './form.svelte';
+import FormError from './formError.svelte';
+import SubmitButton from './submitButton.svelte';
+
+export { Form, FormError, SubmitButton, createForm, createFormData, type SubmitFunction };
+export * from './fields';
+export * from './utils';
