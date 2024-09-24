3bb87bc5397b2a624100451b41ce76dbac5b1069
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-09-24 11:43:34 +0200

    update tests and various fixes

--- saas/{{cookiecutter.project_name}}/webapp/src/routes/[[lang]]/my/organizations/[id]/+page.svelte
+++ saas/{{cookiecutter.project_name}}/webapp/src/routes/[[lang]]/my/organizations/[id]/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import OrganizationLayout from '$lib/components/organizationLayout.svelte';
+
 	export let data;
 	const { organization } = data;
 </script>
 
-<pre>{JSON.stringify(organization, null, 2)}</pre>
+<OrganizationLayout org={organization}>
+	<pre>{JSON.stringify(organization, null, 2)}</pre>
+</OrganizationLayout>
