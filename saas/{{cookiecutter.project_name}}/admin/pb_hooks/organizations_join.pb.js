26a78cc5d0ac3afc2d72c545ad058b72e8a1d3f9
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-09-27 12:03:54 +0200

    refactor: membership requests email

--- saas/{{cookiecutter.project_name}}/admin/pb_hooks/organizations_join.pb.js
+++ saas/{{cookiecutter.project_name}}/admin/pb_hooks/organizations_join.pb.js
@@ -21,119 +21,140 @@ onRecordBeforeCreateRequest((e) => {
     e.record?.set("reminders", 0);
 }, "orgJoinRequests");
 
-/* Email hooks */
+onRecordBeforeCreateRequest((e) => {
+    /** @type {Utils} */
+    const utils = require(`${__hooks}/utils.js`);
+
+    /** @type {string | undefined} */
+    const organizationId = e.record?.get("organization");
+    /** @type {string | undefined} */
+    const userId = e.record?.get("user");
+
+    const authorization = utils.findFirstRecordByFilter(
+        "orgAuthorizations",
+        `organization.id = "${organizationId}" && user.id = "${userId}"`
+    );
+
+    if (authorization)
+        throw new BadRequestError(utils.errors.user_is_already_member);
+}, "orgJoinRequests");
+
+// onRecordAfterUpdateRequest(e => {
+//     const organizationId = e.record?.get("organization")
+// }, "orgAuthorizations")
+
+/* Email hooks - Notifications to Admins */
 
 onRecordAfterCreateRequest((e) => {
     /** @type {Utils} */
     const utils = require(`${__hooks}/utils.js`);
-    /** @type {AuditLogUtils} */
-    const { auditLogger } = require(`${__hooks}/audit-logs.utils.js`);
 
     if (!e.record) throw utils.createMissingDataError("orgJoinRequest");
 
-    try {
-        const organization = utils.getExpanded(e.record, "organization");
-        if (!organization)
-            throw utils.createMissingDataError(
-                "organization of orgJoinRequest"
-            );
-        $app.settings().meta.appUrl;
+    const organization = utils.getExpanded(e.record, "organization");
+    if (!organization)
+        throw utils.createMissingDataError("organization of orgJoinRequest");
 
-        const basePath = $app.isDebug()
-            ? "http://localhost:5173/"
-            : "https://beta.signroom.io/";
+    const organizationId = organization.getId();
+    const organizationName = organization.get("name");
+    const membersUrl = utils.getOrganizationMembersPageUrl(organizationId);
+    const a = `<a href="${membersUrl}">Manage organization join requests</a>`;
 
-        const organizationId = organization.getId();
-        const organizationName = organization.get("name");
-        const acceptanceLink = `<a href="${basePath}my/organizations/${organizationId}/members">Manage organization pending requestes</a>`;
+    const recipients = utils.getOrganizationAdminsAddresses(organizationId);
 
-        const recipients = $app
-            .dao()
-            .findRecordsByFilter(
-                "orgAuthorizations",
-                `organization.id = "${organizationId}" && ( role.name = "admin" || role.name = "owner" )`
-            );
-        $app.dao().expandRecords(recipients, ["user"], undefined);
-
-        /**
-         * @type {mail.Address[]}
-         */
-        const recipientsAddresses = recipients
-            .map((r) => r.expandedOne("user").get("email"))
-            .map((e) => ({ address: e }));
-
-        const message = new MailerMessage({
-            from: {
-                address: $app.settings().meta.senderAddress,
-                name: $app.settings().meta.senderName,
-            },
-            to: recipientsAddresses,
-            subject: `${organizationName} | New join request`,
-            html: `Your organization receved a new join request 
-            <br />
-            ${acceptanceLink}`,
-        });
-        console.log(message.html);
-
-        $app.newMailClient().send(message);
-    } catch (e) {
-        console.log(e);
-        console.log(JSON.stringify(e));
+    const res = utils.sendEmail({
+        to: recipients,
+        subject: `${organizationName} | New join request`,
+        html: `Your organization receved a new join request<br />${a}`,
+    });
+    if (res instanceof Error) {
+        console.error("Email send error");
     }
 }, "orgJoinRequests");
 
-onRecordAfterUpdateRequest((e) => {
-    try {
-        console.log("Hook - orgJoinRequests - Sending email to user");
+cronAdd("remind admins about join requests", "0 9 * * 1", () => {
+    /** @type {Utils} */
+    const utils = require(`${__hooks}/utils.js`);
 
-        const USER_KEY = "user";
-        const ORGANIZATION_KEY = "organization";
+    const organizations = utils.findRecordsByFilter(
+        "organizations",
+        "id != null"
+    );
 
-        const status = e.record.get("status");
-        if (status == "pending") return;
+    organizations
+        .map((organization) => ({
+            organization,
+            requests: utils
+                .findRecordsByFilter(
+                    "orgJoinRequests",
+                    `organization.id = "${organization.getId()}"`
+                )
+                .filter((r) => r.get("status") == "pending"),
+        }))
+        .filter(({ requests }) => requests.length > 0)
+        .forEach(({ organization, requests }) => {
+            const organizationId = organization.getId();
+            const organizationName = organization.get("name");
+            const recipients =
+                utils.getOrganizationAdminsAddresses(organizationId);
+            const membersUrl =
+                utils.getOrganizationMembersPageUrl(organizationId);
+            const a = `<a href="${membersUrl}">Manage membership requests</a>`;
 
-        $app.dao().expandRecord(e.record, [USER_KEY, ORGANIZATION_KEY]);
-
-        const userEmail = e.record.expandedOne(USER_KEY).get("email");
-        /** @type {mail.Address} */
-        const userAddress = { address: userEmail };
-
-        const organizationName = e.record
-            .expandedOne(ORGANIZATION_KEY)
-            .get("name");
-
-        /**
-         * @typedef {Object} EmailContent
-         * @property {string} subject
-         * @property {string} html
-         */
-
-        /** @type {EmailContent} */
-        const successEmail = {
-            subject: `${organizationName} | Request accepted`,
-            html: `Welcome to ${organizationName}! Your request has been accepted!`,
-        };
-        /** @type {EmailContent} */
-        const rejectEmail = {
-            subject: `${organizationName} | Request declined`,
-            html: "Your join request has been declined.",
-        };
-
-        const message = new MailerMessage({
-            from: {
-                address: $app.settings().meta.senderAddress,
-                name: $app.settings().meta.senderName,
-            },
-            to: [userAddress],
-            ...(status == "accepted" ? successEmail : rejectEmail),
+            const res = utils.sendEmail({
+                to: recipients,
+                subject: `${organizationName} | Pending membership requests`,
+                html: `Your organization has ${requests.length} pending membership requests<br />${a}`,
+            });
+            if (res instanceof Error) {
+                console.error("Email send error");
+            }
         });
+});
 
-        $app.newMailClient().send(message);
+/* Email hooks - Notifications to Users */
 
-        // Deleting orgJoinRequest record once all has been done
-        $app.dao().deleteRecord(e.record);
-    } catch (e) {
-        console.log(e);
-        console.log(JSON.stringify(e));
-    }
+onRecordAfterUpdateRequest((e) => {
+    /** @type {Utils} */
+    const utils = require(`${__hooks}/utils.js`);
+
+    if (!e.record) throw utils.createMissingDataError("orgJoinRequest");
+
+    /** @type {string} */
+    const status = e.record.get("status");
+
+    const isRelevantChange = status == "accepted" || status == "rejected";
+    if (!isRelevantChange) return;
+
+    const organization = utils.getExpanded(e.record, "organization");
+    if (!organization)
+        throw utils.createMissingDataError("organization of orgJoinRequest");
+    const user = utils.getExpanded(e.record, "user");
+    if (!user) throw utils.createMissingDataError("user of orgJoinRequest");
+
+    /** @type {string} */
+    const organizationName = organization.get("name");
+    const userAddress = utils.getUserEmailAddressData(user);
+
+    /**
+     * @typedef {Object} EmailContent
+     * @property {string} subject
+     * @property {string} html
+     */
+
+    /** @type {EmailContent} */
+    const successEmail = {
+        subject: `${organizationName} | Membership request accepted`,
+        html: `Welcome to ${organizationName}! Your request has been accepted!`,
+    };
+    /** @type {EmailContent} */
+    const rejectEmail = {
+        subject: `${organizationName} | Membership request declined`,
+        html: "Your join request has been declined.",
+    };
+
+    const emailContent = status == "accepted" ? successEmail : rejectEmail;
+    utils.sendEmail({ to: [userAddress], ...emailContent });
+
+    $app.dao().deleteRecord(e.record);
 }, "orgJoinRequests");
