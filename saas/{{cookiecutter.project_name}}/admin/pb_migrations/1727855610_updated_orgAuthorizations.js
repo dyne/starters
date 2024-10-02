0d4ba6628db87808d9c72f65a899d46eda449ab9
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-10-02 09:54:39 +0200

    feat: add cascade delete for users

---
+++ saas/{{cookiecutter.project_name}}/admin/pb_migrations/1727855610_updated_orgAuthorizations.js
@@ -0,0 +1,48 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("k1vlx34o1x8tzno")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "hlla24j5",
+    "name": "user",
+    "type": "relation",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": true,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("k1vlx34o1x8tzno")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "hlla24j5",
+    "name": "user",
+    "type": "relation",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
