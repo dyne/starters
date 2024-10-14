315b70d36d161a4e64efa711319f5c2c57d89341
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-10-14 02:44:42 +0200

    feat: yao

--- saas/{{cookiecutter.project_name}}/webapp/src/modules/pocketbase/zod-schema/index.ts
+++ saas/{{cookiecutter.project_name}}/webapp/src/modules/pocketbase/zod-schema/index.ts
@@ -7,34 +7,15 @@ import { getCollectionModel } from '@/pocketbase/collections-models';
 import { isArrayField } from '@/pocketbase/collections-models/utils';
 
 import { pipe } from 'effect';
-import * as TF from 'type-fest';
 import type { CollectionFormData } from '@/pocketbase/types';
 
 //
 
-type ZodExtend = Record<string, z.ZodTypeAny>;
+// Note: a bit of a type hack
 
-type InferZodExtend<E extends ZodExtend> = {
-	[K in keyof E]: z.infer<E[K]>;
-};
-
-export function createCollectionZodSchema<
-	C extends CollectionName,
-	Exclude extends (keyof CollectionFormData[C])[],
-	Extend extends ZodExtend,
-	CollectionZodSchema = z.ZodType<
-		TF.Simplify<
-			TF.OmitIndexSignature<Omit<CollectionFormData[C], Exclude[number]> & InferZodExtend<Extend>>
-		>
-	>
->(
-	collection: C,
-	options: {
-		exclude?: Exclude;
-		extend?: Extend;
-	} = {}
-): CollectionZodSchema {
-	options;
+export function createCollectionZodSchema<C extends CollectionName>(
+	collection: C
+): CollectionZodSchema<C> {
 	const { schema } = getCollectionModel(collection);
 	const entries = schema.map((fieldConfig) => {
 		const zodTypeConstructor = fieldConfigToZodTypeMap[fieldConfig.type] as FieldConfigToZodType;
@@ -58,49 +39,35 @@ export function createCollectionZodSchema<
 		return [fieldConfig.name, zodType];
 	});
 	const rawObject = Object.fromEntries(entries);
-	return z.object(rawObject) as CollectionZodSchema;
+	return z.object(rawObject) as CollectionZodSchema<C>;
 }
 
-const s = createCollectionZodSchema('z_test_collection', {
-	exclude: [
-		'text_field',
-		'file_multi_field',
-		'relation_field',
-		'url_field',
-		'boolean_field',
-		'number_field',
-		'select_field',
-		'select_multi_field'
-	],
-	extend: {
-		gino: z.string()
-	}
-});
+const s = createCollectionZodSchema('z_test_collection');
 
 // eslint-disable-next-line
 const y = s.parse({});
 
-// type CollectionZodSchema<C extends keyof CollectionFormData> = z.ZodObject<{
-// 	[K in keyof CollectionFormData[C]]: TypetoZodType<CollectionFormData[C][K]>;
-// }>;
+type CollectionZodSchema<C extends keyof CollectionFormData> = z.ZodObject<{
+	[K in keyof CollectionFormData[C]]-?: TypeToZodOptionalType<CollectionFormData[C][K]>;
+}>;
 
-// type TypetoZodType<T> = T extends File
-// 	? z.ZodType<File>
-// 	: T extends File[]
-// 		? z.ZodArray<z.ZodType<File>>
-// 		: T extends number
-// 			? z.ZodNumber
-// 			: T extends boolean
-// 				? z.ZodBoolean
-// 				: T extends string
-// 					? z.ZodString
-// 					: T extends string[]
-// 						? z.ZodArray<z.ZodString>
-// 						: T extends unknown
-// 							? unknown extends T // ChatGPT: How to match exactly unknown
-// 								? z.ZodUnknown
-// 								: never
-// 							: never;
+type TypeToZodOptionalType<T> =
+	T extends NonNullable<T> ? TypetoZodType<T> : z.ZodOptional<TypetoZodType<T>>;
 
-// type U = CollectionFormData['z_test_collection'];
-// type N = CollectionZodSchema<'z_test_collection'>;
+type TypetoZodType<T> = T extends File
+	? z.ZodType<File>
+	: T extends File[]
+		? z.ZodArray<z.ZodType<File>>
+		: T extends number
+			? z.ZodNumber
+			: T extends boolean
+				? z.ZodBoolean
+				: T extends string
+					? z.ZodString
+					: T extends string[]
+						? z.ZodArray<z.ZodString>
+						: T extends unknown
+							? unknown extends T // ChatGPT: How to match exactly unknown
+								? z.ZodUnknown
+								: never
+							: never;
